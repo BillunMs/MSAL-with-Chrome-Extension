040552a96d4cb7d65c7106346fb614b045f046b9
Serzhanov Nurbek <ns490367@gmail.com>
2023-01-06 23:45:56 +0100

    regenerating

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.static(__dirname + '/dist/microsoft-login'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/microsoft-login/index.html'));});
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8000);
