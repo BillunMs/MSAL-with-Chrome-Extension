07bbddd1e7eded9eba13bad841edc7299dcbbcee
Serzhanov Nurbek <ns490367@gmail.com>
2023-01-06 22:45:18 +0100

    updating

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  messagesAPI_MS:'https://graph.microsoft.com/v1.0/me/messages',
+  emailList:'https://billun.com/api/emails',
+  categoriesAPI:'https://graph.microsoft.com/beta/me/outlook/masterCategories',
+  sendDataBillun:'https://billun.com/api/Site/Declare'
 };
